9593a564299e501c96c95b2ddd85b728deba3823
aosunathepoet <aosunathepoet@gmail.com>
2021-02-24 06:24:07 -0500

    Post activity integrated

--- app/src/main/java/com/example/instaclone/MainActivity.java
+++ app/src/main/java/com/example/instaclone/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.instaclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    private EditText etDescription;
+    private Button btnCaptureImage;
+    private ImageView ivPostPicture;
+    private Button btnPublishPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etDescription = findViewById(R.id.etDescription);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        ivPostPicture = findViewById(R.id.ivPostPicture);
+        btnPublishPost = findViewById(R.id.btnPublishPost);
+
+
     }
 }
--- app/src/main/java/com/example/instaclone/ParseApplication.java
+++ app/src/main/java/com/example/instaclone/ParseApplication.java
@@ -3,12 +3,16 @@ package com.example.instaclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("ubxt962DMPsjcWbC99jZuvEa5uI8Auxe27RuJ72z")
